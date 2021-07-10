2f642a53e1bdcd1fa0204b8dec19f57f9d6e1d76
semiCOLON <84295513+semicolon0401@users.noreply.github.com>
2021-07-10 10:58:04 +0900

    Update binary-calculator.js

--- binary-calculator.js
+++ binary-calculator.js
@@ -1,8 +1,4 @@
-var num = prompt("이진수로 바꿀 수를 입력하시오(주의 : 무조건 자연수만 입력하시오!)");
-var now_num = num;
 var result = "";
-while (now_num != 1) {
-    result = (now_num % 2) + result;
-    now_num = parseInt(now_num / 2);
-}
-alert(num + "을 이진수로 바꾼 수는 1" + result + "입니다");
+for(var num = prompt("이진수로 바꿀 수를 입력하시오"); num > 1; num = parseInt(num / 2))
+    result = (num % 2) + result;
+alert("1" + result);
